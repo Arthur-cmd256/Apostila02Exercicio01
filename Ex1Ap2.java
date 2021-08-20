ab3b26ce1d15cb709f60ca2b5cb277fbe89000ab
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-08-19 21:35:20 -0300

    Algoritimo pronto

--- Ex1Ap2.java
+++ Ex1Ap2.java
@@ -4,7 +4,19 @@ public class Ex1Ap2{
 
     public static void main(String[] args) {
         
-        
+        Double salario;
+
+        salario = Double.parseDouble(JOptionPane.showInputDialog("Digite seu salario, com os centavos: "));
+
+        if(salario == 1100){
+            JOptionPane.showMessageDialog(null, "Voce ganha um salario minimo");
+        }
+        else if(salario > 1100){
+            JOptionPane.showMessageDialog(null, "Voce ganha mais de um salario minimo");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Voce ganha menos de um salario minimo");
+        }
 
     }
 
